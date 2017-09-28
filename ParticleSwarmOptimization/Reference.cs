21835500345bc1972a07b6207d51856704ee0257
Adriano Costa <adriano.kosta@yahoo.com.br>
2017-09-28 00:24:02 -0300

    First Commit

---
+++ ParticleSwarmOptimization/Reference.cs
@@ -0,0 +1,9 @@
+using System;
+
+public static class Reference{
+    public static int[] Time = new int[]{8,8,2,8,8,8,2,8,8,4,2,4,2,8,8,2,16,16,16,16};
+     
+    public static char[] Note = new char[]{'d','e','f','e','d','e','a','g','f','d','b','b','a','d','e','f','g','f','e','d'};
+
+    public static int totalTime = 180;
+}
